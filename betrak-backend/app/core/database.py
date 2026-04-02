1eb233478a1870da103a7a8bed00cbac54819718
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-04-02 20:36:32 +0600

    Update the database core

--- betrak-backend/app/core/database.py
+++ betrak-backend/app/core/database.py
@@ -1,8 +1,13 @@
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from app.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=false, autoflush=false, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
 
 def get_db():
     db = SessionLocal()

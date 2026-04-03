3068428263bf8d4ccf44c92c2a2e096a76bab48c
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-04-03 22:38:07 +0600

    Add user input schemas

---
+++ betrak-backend/app/api/v1/schemas/user_input.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+class MentalHealthAnswers(BaseModel):
+    answer_1: Literal["never", "sometimes", "often", "always"]
+    answer_2: Literal["very_happy", "neutral", "sometimes_sad", "mostly_sad"]
+    answer_3: Literal["never", "rarely", "often", "always"]
+
+class UserInput(BaseModel):
+    age: int = Field(..., ge=10, le=100)
+    gender: Literal["Male", "Female"]
+    country: str = Field(..., min_length=2, max_length=100)
+    avg_daily_usage_hours: float = Field(..., ge=0.0, le=24.0)
+    most_used_platform: Literal[
+        "Instagram",
+        "TikTok",
+        "Facebook",
+        "Twitter",
+        "Snapchat",
+        "YouTube",
+        "LinkedIn",
+        "WhatsApp"
+    ]
+    sleep_hours_per_night: float = Field(..., ge=0.0, le=24.0)
+    mental_health_answers = MentalHealthAnswers

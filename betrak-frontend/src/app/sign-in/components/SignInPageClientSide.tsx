1204a9ac78166d3caef70ef3bfa47f2d211b4fa5
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-04-11 00:20:13 +0600

    Add SignInPageClientSide component

---
+++ betrak-frontend/src/app/sign-in/components/SignInPageClientSide.tsx
@@ -0,0 +1,10 @@
+"use client"
+const SignInPageClientSide = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SignInPageClientSide;
--- betrak-frontend/src/app/sign-in/page.tsx
+++ betrak-frontend/src/app/sign-in/page.tsx
@@ -1,7 +1,10 @@
-
+import { Suspense } from "react";
+import SignInPageClientSide from "./components/SignInPageClientSide";
 
 export default function SignInPage () {
     return (
-
+        <Suspense fallback={<p>laoding....</p>}>
+            <SignInPageClientSide />
+        </Suspense>
     )
 }

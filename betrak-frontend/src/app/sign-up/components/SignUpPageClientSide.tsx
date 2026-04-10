3d6b27f7f89c3c45708b799e9f4c76341783940e
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-04-11 00:35:30 +0600

    Update the Google button of sign-in and sign-up page

--- betrak-frontend/src/app/sign-up/components/SignUpPageClientSide.tsx
+++ betrak-frontend/src/app/sign-up/components/SignUpPageClientSide.tsx
@@ -5,6 +5,7 @@ import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import SignUpFormFields from "./SignUpFormFields";
+import { FcGoogle } from "react-icons/fc";
 
 const SignUpPageClientSide = () => {
   const searchParams = useSearchParams();
@@ -36,21 +37,7 @@ const SignUpPageClientSide = () => {
             type="button"
             className=" bg-indigo-500/70 text-white hover:bg-indigo-600/70 py-6 btn-outline rounded-lg font-semibold text-[16px] w-full"
           >
-            <svg aria-label="Google logo"
-              width="20" height="20"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <g> <path d="m0 0H512V512H0" fill="#fff"></path>
-                <path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                ></path>
-                <path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                ></path>
-                <path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                ></path>
-                <path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                ></path> </g>
-            </svg>
+            <FcGoogle />
             Sign up with Google
           </Button>
 

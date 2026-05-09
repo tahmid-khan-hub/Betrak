d354e0ba72018bb5d1b171de3d63c42dc9c62830
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-05-09 20:00:32 +0600

    Create HistoryType interface, data fetch in ProgressChart

--- betrak-frontend/src/app/result/components/StatsCharts/components/ProgressChart.tsx
+++ betrak-frontend/src/app/result/components/StatsCharts/components/ProgressChart.tsx
@@ -1,6 +1,23 @@
 "use client";
+import { HistoryType } from "@/types/HistoryType";
+import { useQuery } from "@tanstack/react-query";
+
+const levelColor = {
+  high: "bg-red-500/20 text-red-400 border-red-500/30",
+  medium: "bg-yellow-500/20 text-yellow-400 border-yellow-500/30",
+  low: "bg-green-500/20 text-green-400 border-green-500/30",
+}
 
 const ProgressChart = () => {
+    const {data, isLoading} = useQuery({
+        queryKey: ["history"],
+        queryFn: async () => {
+            const res = await fetch ("/api/result/history")
+            return res.json();
+        }
+    })
+
+    const historyData: HistoryType[] = data?.data ?? [];
     return (
         <div>
             
---
+++ betrak-frontend/src/types/HistoryType.ts
@@ -0,0 +1,8 @@
+export interface HistoryType {
+  addiction_level: string;
+  confidence: number;
+  avg_daily_usage_hours: number;
+  sleep_hours_per_night: number;
+  mental_health_score: number;
+  created_at: string;
+}
